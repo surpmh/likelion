e1be12f8a0d141c281a5098256650a249b58b26a
surpmh <cdd8816@gmail.com>
2022-10-05 18:03:09 +0900

    코드업 기초 100제

---
+++ src/codeup/problem1020/Main.java
@@ -0,0 +1,11 @@
+package codeup.problem1020;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String num = sc.next();
+        System.out.println(num.replace("-", ""));
+    }
+}
