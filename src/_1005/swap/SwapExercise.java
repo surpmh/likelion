3c2e5fbb6fa55d104fee984c49174f7c61fa357f
surpmh <cdd8816@gmail.com>
2022-10-05 18:28:07 +0900

    a와 b를 바꾸는 로직

---
+++ src/_1005/swap/SwapExercise.java
@@ -0,0 +1,15 @@
+package _1005.swap;
+
+public class SwapExercise {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int temp = a;
+
+        a = b;
+        b = temp;
+
+        System.out.printf("a:%d b:%d", a, b);
+    }
+}
