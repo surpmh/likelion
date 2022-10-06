e8dfe9df49c183fd41d872d4e05ea6665286006c
surpmh <cdd8816@gmail.com>
2022-10-06 10:42:47 +0900

    중복 없는 랜덤 알파벳 생성

---
+++ src/_1006/randomalphabet/AlphabetGenerator.java
@@ -0,0 +1,5 @@
+package _1006.randomalphabet;
+
+public interface AlphabetGenerator {
+    int generate();
+}
---
+++ src/_1006/randomalphabet/RandomAlphabetGenerator.java
@@ -0,0 +1,7 @@
+package _1006.randomalphabet;
+
+public class RandomAlphabetGenerator {
+    public int generate() {
+        return (int)(Math.random() * 27) + 65;
+    }
+}
