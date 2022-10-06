b0a10e8ffc7c15340300476a8d4ec6cbc242afe8
surpmh <cdd8816@gmail.com>
2022-10-06 14:42:22 +0900

    파일 한 글자 읽기

--- src/_1006/file/ReadFile1.java
+++ src/_1006/file/ReadFile1.java
@@ -1,10 +1,9 @@
 package _1006.file;
 
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class ReadFile {
+public class ReadFile1 {
     public static void main(String[] args) throws IOException {
         FileReader reader = new FileReader("./src/_1006/file/test_file.txt");
         
