57ea5397af50fe942555dc6e3f0cca474507937b
surpmh <cdd8816@gmail.com>
2022-10-06 21:23:20 +0900

    CodeUp 1011~1020

---
+++ src/codeup/day2/problem1011/Main.java
@@ -0,0 +1,11 @@
+package codeup.day2.problem1011;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char str = sc.nextLine().charAt(0);
+        System.out.println(str);
+    }
+}
