0500db3d0af870d1eeadff5df9f7e93a2dcda770
surpmh <cdd8816@gmail.com>
2022-10-08 20:25:46 +0900

    인구 이동 데이터

---
+++ src/_1007/PopulationMove.java
@@ -0,0 +1,18 @@
+package _1007;
+
+public class PopulationMove {
+    int fromCity;
+    int toCity;
+    public PopulationMove(int fromCity, int toCity) {
+        this.fromCity = fromCity;
+        this.toCity = toCity;
+    }
+
+    public int getFromCity() {
+        return fromCity;
+    }
+
+    public int getToCity() {
+        return toCity;
+    }
+}
