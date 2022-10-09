f03b7bda0974439b75d3808bc95fe5eafba02b10
surpmh <cdd8816@gmail.com>
2022-10-09 12:05:18 +0900

    CodeUp 1031~1040

---
+++ src/codeup/day4/problem1032/Main.java
@@ -0,0 +1,11 @@
+package codeup.day4.problem1032;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.printf("%x", num);
+    }
+}
