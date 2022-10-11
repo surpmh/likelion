c4e0d72e89389322b8452568eae049426c04f876
surpmh <cdd8816@gmail.com>
2022-10-11 18:31:45 +0900

    CodeUp 1041~1050

--- src/codeup/day4/problem1037/Main.java
+++ src/codeup/day4/problem1037/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        System.out.print((char)num);
+        System.out.println((char)num);
     }
 }
