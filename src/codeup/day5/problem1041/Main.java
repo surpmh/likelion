57f2efe272aac91b29380b353de43a3ca46d1a5c
surpmh <cdd8816@gmail.com>
2022-10-11 18:21:15 +0900

    CodeUp 1041 ~ 1050

--- src/codeup/day5/problem1041/Main.java
+++ src/codeup/day5/problem1041/Main.java
@@ -1,11 +1,11 @@
-package codeup.day4.problem1037;
+package codeup.day5.problem1041;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        System.out.println((char)num);
+        char ch = sc.nextLine().charAt(0);
+        System.out.println((char)(ch+1));
     }
 }
---
+++ src/codeup/day5/problem1049/Main.java
@@ -0,0 +1,12 @@
+package codeup.day5.problem1049;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a > b ? 1 : 0);
+    }
+}
