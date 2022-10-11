ba2276bfc4affba48fbcab23950fd6f779ca8d2f
surpmh <cdd8816@gmail.com>
2022-10-11 10:14:19 +0900

    코드업 1620 : 자릿수의 합

---
+++ src/_1011/digits/DigitsSum.java
@@ -0,0 +1,25 @@
+package _1011.digits;
+
+import java.util.Scanner;
+
+public class DigitsSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        while (true) {
+            sum = 0;
+            while (n != 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            if (sum / 10 == 0){
+                break;
+            } else {
+                n = sum;
+            }
+        }
+        System.out.println(sum);
+    }
+}
