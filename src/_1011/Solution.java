44591761b86f10d4f8530942720b63d2452c4e10
surpmh <cdd8816@gmail.com>
2022-10-11 09:35:31 +0900

    프로그래머스 자릿수 더하기

--- src/_1011/Solution.java
+++ src/_1011/Solution.java
@@ -1,12 +1,25 @@
 package _1011;
 
+//public class Solution {
+//    public int solution(int n) {
+//        int answer = 0;
+//        String s = String.valueOf(n);
+//
+//        for (int i=0; i<s.length(); i++) {
+//            answer += Integer.parseInt(String.valueOf(s.charAt(i)));
+//        }
+//
+//        return answer;
+//    }
+//}
+
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String s = String.valueOf(n);
 
-        for (int i=0; i<s.length(); i++) {
-            answer += Integer.parseInt(String.valueOf(s.charAt(i)));
+        while (n != 0) {
+            answer += n % 10;
+            n /= 10;
         }
 
         return answer;
