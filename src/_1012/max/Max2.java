9973bd123414c75bfa926c101ee89184d3a7d92f
surpmh <cdd8816@gmail.com>
2022-10-12 10:43:26 +0900

    코드업 4596 : 최대값 2

--- src/_1012/max/Max2.java
+++ src/_1012/max/Max2.java
@@ -7,26 +7,19 @@ public class Max2 {
         Scanner sc = new Scanner(System.in);
         int[][] arr = new int[9][9];
         int max = 0;
+        int[] maxidx = new int[2];
 
         for (int i=0; i<9; i++) {
             for (int j=0; j<9; j++) {
                 arr[i][j] = sc.nextInt();
                 if (max < arr[i][j]) {
                     max = arr[i][j];
+                    maxidx[0] = i+1;
+                    maxidx[1] = j+1;
                 }
             }
         }
-
         System.out.println(max);
-
-        Loop:
-        for (int i=0; i<9; i++) {
-            for (int j=0; j<9; j++) {
-                if (arr[i][j] == max) {
-                    System.out.print((i+1) + " " + (j+1));
-                    break Loop;
-                }
-            }
-        }
+        System.out.println(maxidx[0] + " " + maxidx[1]);
     }
 }
