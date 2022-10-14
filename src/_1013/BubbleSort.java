895adbaf9cc4256d3f48cfca2adf8d18fffa6001
surpmh <cdd8816@gmail.com>
2022-10-14 18:26:03 +0900

    정렬

--- src/_1013/BubbleSort.java
+++ src/_1013/BubbleSort.java
@@ -1,9 +1,9 @@
-package _1013.sort;
+package _1013;
 
 import java.util.Arrays;
 
 public class BubbleSort {
-    public int[] sort(int[] arr) {
+    public void sort(int[] arr) {
         for (int i=0; i<arr.length-1; i++) {
             for (int j=i+1; j<arr.length; j++) {
                 if(arr[i] > arr[j]) {
@@ -13,7 +13,6 @@ public class BubbleSort {
                 }
             }
         }
-        return arr;
     }
 
     public static void main(String[] args) {
