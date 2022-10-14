86c6fb73e6b53406ed98b21408b3c0da25c724b2
surpmh <cdd8816@gmail.com>
2022-10-14 11:39:27 +0900

    InsertQueryTest

--- src/main/java/hospital/domain/Hospital.java
+++ src/main/java/hospital/domain/Hospital.java
@@ -12,12 +12,38 @@ public class Hospital {
     public Hospital(String id, String address, String category, Integer emergencyRoom, String name, String subdivision) {
         this.id = id;
         this.address = address;
-        String[] splitted = this.address.split(" ");
-        this.district = String.format("%s %s", splitted[0], splitted[1]);
+        this.setDistrict();
         this.category = category;
         this.emergencyRoom = emergencyRoom;
         this.name = name;
         this.subdivision = subdivision;
+    }
+
+    private void setDistrict() {
+        String[] splitted = this.address.split(" ");
+        this.district = String.format("%s %s", splitted[0], splitted[1]);
+    }
+
+    public String getSqlInsertQuery() {
+        String sql = String.format("INSERT INTO `likelion-db`.`seoul_hospital`\n" +
+                "(`id`,`address`,`district`,`category`,`emergency_room`,`name`,`subdivision`)\n"+
+                "VALUES\n" +
+                "(\"%s\",\n" +
+                "\"%s\",\n" +
+                "\"%s\",\n" +
+                "\"%s\",\n" +
+                "%d,\n" +
+                "\"%s\",\n" +
+                "\"%s\");", this.id, this.address, this.district, this.category, this.emergencyRoom, this.name, this.subdivision);
+        return sql;
+    }
+
+    public String getTupleString() {
+        String sql = String.format(
+                "(\"%s\",\"%s\"," +"\"%s\"," +
+                        "\"%s\"," +"%d," +"\"%s\"," +"\"%s\");",
+                this.id, this.address, this.district, this.category, this.emergencyRoom, this.name, this.subdivision);
+        return sql;
     }
 
     public String getId() {
