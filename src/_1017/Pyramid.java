293624cf0264b3ba4c656ff87a8b66c07956d6f7
surpmh <cdd8816@gmail.com>
2022-10-17 10:33:15 +0900

    피라미드 별찍기

--- src/_1017/Pyramid.java
+++ src/_1017/Pyramid.java
@@ -14,10 +14,18 @@ public class Pyramid {
             System.out.println();
         }
     }
+
+    public void oddstar(int n) {
+        for (int i=0; i<n; i++) {
+            System.out.println(" ".repeat(n-i-1) + "*".repeat(2*i+1));
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Pyramid pyramid = new Pyramid();
         int n = sc.nextInt();
-        pyramid.star(n);
+        //pyramid.star(n);
+        pyramid.oddstar(n);
     }
 }
