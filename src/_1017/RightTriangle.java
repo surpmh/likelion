7b59f15c1de556abf430415de0c90dcf89388307
surpmh <cdd8816@gmail.com>
2022-10-17 09:09:15 +0900

    별찍기

--- src/_1017/RightTriangle.java
+++ src/_1017/RightTriangle.java
@@ -3,8 +3,8 @@ package _1017;
 public class RightTriangle {
     public static void main(String[] args) {
         System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
     }
 }
