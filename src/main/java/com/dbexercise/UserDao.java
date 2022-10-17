363b5df9de8121eb595945467a0b2af8b5164082
surpmh <cdd8816@gmail.com>
2022-10-17 13:48:29 +0900

    SELECT

--- src/main/java/com/dbexercise/UserDao.java
+++ src/main/java/com/dbexercise/UserDao.java
@@ -1,5 +1,7 @@
 package com.dbexercise;
 
+import com.dbexercise.domain.User;
+
 import java.sql.*;
 import java.util.Map;
 
@@ -22,8 +24,29 @@ public class UserDao {
         conn.close();
     }
 
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+        PreparedStatement ps = conn.prepareStatement("SELECT * FROM users WHERE id = ?");
+        ps.setString(1, id);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+        rs.close();
+        ps.close();
+        conn.close();
+        return user;
+    }
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
-        userDao.add();
+        // userDao.add();
+        User user = userDao.get("1");
+        System.out.println(user.getName());
     }
 }
