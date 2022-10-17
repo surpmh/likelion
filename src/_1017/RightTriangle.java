9505c66df3b67e02461657b0ab4b1a7864730ded
surpmh <cdd8816@gmail.com>
2022-10-17 09:25:42 +0900

    별찍기

--- src/_1017/RightTriangle.java
+++ src/_1017/RightTriangle.java
@@ -1,12 +1,21 @@
 package _1017;
 
+import java.util.Scanner;
+
 public class RightTriangle {
-    public static void main(String[] args) {
-        for (int i=1; i<=4; i++) {
-            for (int j=0; j<i; j++) {
+    public void star(int n) {
+        for (int i=0; i<4; i++) {
+            for (int j=0; j<=i; j++) {
                 System.out.print("* ");
             }
-                System.out.println();
+            System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        RightTriangle rightTriangle = new RightTriangle();
+        int num = sc.nextInt();
+        rightTriangle.star(num);
     }
 }
