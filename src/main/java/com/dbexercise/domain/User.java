be5b452536a1bfa8f7eb22c40d82cd0d50f41973
surpmh <cdd8816@gmail.com>
2022-10-18 11:01:38 +0900

    User

---
+++ src/main/java/com/dbexercise/domain/User.java
@@ -0,0 +1,40 @@
+package com.dbexercise.domain;
+
+public class User {
+    private String id;
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
