c5d06245b0f9e84e918d52a97b94a849a84154b8
surpmh <cdd8816@gmail.com>
2022-10-18 09:11:20 +0900

    정사각형 별찍기

--- src/_1018/Square.java
+++ src/_1018/Square.java
@@ -3,15 +3,19 @@ package _1018;
 import java.util.Scanner;
 
 public class Square {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
+    public void printSuare(int n) {
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Square square = new Square();
+        square.printSuare(n);
     }
 }
