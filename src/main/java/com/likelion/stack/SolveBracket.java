0b8a2b4c6ff8dc4688313dd9540f5beb3a991db2
surpmh <cdd8816@gmail.com>
2022-10-21 10:25:05 +0900

    괄호 풀기

--- src/main/java/com/likelion/stack/SolveBracket.java
+++ src/main/java/com/likelion/stack/SolveBracket.java
@@ -1,10 +1,21 @@
 package com.likelion.stack;
 
+import java.util.Arrays;
+
 public class SolveBracket {
-    public boolean solution(String s) {
+    public boolean solution1(String s) {
         while (s.indexOf("()") >= 0) {
             s = s.replace("()", "");
         }
+        return s.length() == 0;
+    }
+
+    public boolean solution2(String s) {
+        while (s.indexOf("()") >= 0) {
+            String[] splitted = s.split("\\(\\)");
+            s = String.join("", splitted);
+        }
+
         return s.length() == 0;
     }
 }
--- src/test/java/com/likelion/stack/SolveBracketTest.java
+++ src/test/java/com/likelion/stack/SolveBracketTest.java
@@ -11,8 +11,13 @@ class SolveBracketTest {
     @DisplayName("괄호가 잘 풀리는지")
     void solution() {
         SolveBracket sb = new SolveBracket();
+        SolveBracketStack sbs = new SolveBracketStack();
 
-        assertTrue(sb.solution("(())()"));
-        assertFalse(sb.solution(")()("));
+        assertTrue(sb.solution1("(())()"));
+        assertFalse(sb.solution1(")()("));
+        assertTrue(sb.solution2("(())()"));
+        assertFalse(sb.solution2(")()("));
+        assertTrue(sbs.solution("(())()"));
+        assertFalse(sbs.solution(")()("));
     }
 }
