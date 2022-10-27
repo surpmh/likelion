02052df9c54b8beb6fb9cd14adf97ae87553ed9a
surpmh <cdd8816@gmail.com>
2022-10-27 15:07:47 +0900

    Put ResponseEntity

--- src/main/java/com/springboot/hello/controller/PutController.java
+++ src/main/java/com/springboot/hello/controller/PutController.java
@@ -1,6 +1,8 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,5 +19,13 @@ public class PutController {
     @PutMapping(value = "/member2")
     public MemberDto postMemberDto2(@RequestBody MemberDto memberDto) {
         return memberDto;
+    }
+
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto>
+            putMembeer(@RequestBody MemberDto memberDto) {
+                return ResponseEntity
+                        .status(HttpStatus.ACCEPTED)
+                        .body(memberDto);
     }
 }
