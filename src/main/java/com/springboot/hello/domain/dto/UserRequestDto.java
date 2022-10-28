c1a2cbcfd14102ecff4aa6431e0d62071ac07b34
surpmh <cdd8816@gmail.com>
2022-10-28 15:13:24 +0900

    add: UserRequestDto

---
+++ src/main/java/com/springboot/hello/domain/dto/UserRequestDto.java
@@ -0,0 +1,12 @@
+package com.springboot.hello.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserRequestDto {
+    private String id;
+    private String name;
+    private String password;
+}
