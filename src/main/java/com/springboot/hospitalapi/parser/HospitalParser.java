de21e9b3682323c9beae25710bf332fd414702cb
surpmh <cdd8816@gmail.com>
2022-11-06 21:29:33 +0900

    Add: Hospital API

---
+++ src/main/java/com/springboot/hospitalapi/parser/HospitalParser.java
@@ -0,0 +1,36 @@
+package com.springboot.hospitalapi.parser;
+
+import com.springboot.hospitalapi.domain.Hospital;
+
+import java.time.LocalDateTime;
+
+public class HospitalParser implements Parser<Hospital> {
+
+    @Override
+    public Hospital parse(String str) {
+        String[] row = str.split("\",\"");
+
+        Hospital hospital = new Hospital();
+        hospital.setId(Integer.parseInt(row[0].replace("\"", "")));
+        hospital.setOpenServiceName(row[1]);
+        hospital.setOpenLocalGovernmentCode(Integer.parseInt(row[3]));
+        hospital.setManagementNumber(row[4]);
+        int year = Integer.parseInt(row[5].substring(0, 4));
+        int month = Integer.parseInt(row[5].substring(4, 6));
+        int day = Integer.parseInt(row[5].substring(6, 8));
+        hospital.setLicenseDate(LocalDateTime.of(year, month, day, 0, 0, 0));
+        hospital.setBusinessStatus(Integer.parseInt(row[7]));
+        hospital.setBusinessStatusCode(Integer.parseInt(row[9]));
+        hospital.setPhone(row[15]);
+        hospital.setFullAddress(row[18]);
+        hospital.setRoadNameAddress(row[19]);
+        hospital.setHospitalName(row[21]);
+        hospital.setBusinessTypeName(row[25]);
+        hospital.setHealthcareProviderCount(Integer.parseInt(row[29]));
+        hospital.setPatientRoomCount(Integer.parseInt(row[30]));
+        hospital.setTotalNumberOfBeds(Integer.parseInt(row[31]));
+        hospital.setTotalAreaSize(Float.parseFloat(row[32]));
+
+        return hospital;
+    }
+}
---
+++ src/main/java/com/springboot/hospitalapi/parser/ReadLineContext.java
@@ -0,0 +1,35 @@
+package com.springboot.hospitalapi.parser;
+
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadLineContext<T> {
+    private final Parser<T> parser;
+
+    public ReadLineContext(Parser<T> parser) {
+        this.parser = parser;
+    }
+
+    public List<T> readByLine(String filename) throws IOException {
+        // 삽
+        List<T> result = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(
+                new FileReader(filename)
+        );
+        String str;
+        while ((str = reader.readLine()) != null) {
+            try {
+                result.add(parser.parse(str));
+            } catch (Exception e) {
+                System.out.printf("파싱중 문제가 생겨 이 라인은 넘어갑니다. 파일내용:%s", str);
+            }
+        }
+        reader.close();
+        return result;
+    }
+}
