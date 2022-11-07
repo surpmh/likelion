c175e4f55b1b4c766e35dbd38b198a00b4e419e2
surpmh <cdd8816@gmail.com>
2022-11-07 15:43:56 +0900

    feat: ArticleRepository 추가

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.mustache.bbs.controller;
 
 import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.domain.entity.Article;
+import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 @Slf4j
 public class ArticleController {
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping(value = "new")
     public String newArticleForm() {
         return "articles/new";
@@ -21,6 +28,7 @@ public class ArticleController {
     public String createArticle(ArticleDto form) {
         log.info(form.toString());      // 로그 남기기
         Article article = form.toEntity();
+        articleRepository.save(article);
         return "";
     }
 }
