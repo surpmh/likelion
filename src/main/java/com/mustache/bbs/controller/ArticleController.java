5094bdbf1e8d252a18484b67bf133e7025bb433a
surpmh <cdd8816@gmail.com>
2022-11-07 14:29:37 +0900

    Add: ArticleController

---
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -0,0 +1,24 @@
+package com.mustache.bbs.controller;
+
+import com.mustache.bbs.domain.dto.ArticleDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/articles")
+@Slf4j
+public class ArticleController {
+    @GetMapping(value = "new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping(value = "posts")
+    public String createArticle(ArticleDto form) {
+        log.info(form.toString());
+        return "";
+    }
+}
