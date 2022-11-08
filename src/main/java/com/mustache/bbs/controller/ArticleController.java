6b95922c869c209c7b6f8775dbd5a4aeb0528a28
surpmh <cdd8816@gmail.com>
2022-11-08 15:01:24 +0900

    feat: 조회 기능 추가(오류)

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -5,9 +5,13 @@ import com.mustache.bbs.domain.entity.Article;
 import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -19,16 +23,28 @@ public class ArticleController {
         this.articleRepository = articleRepository;
     }
 
-    @GetMapping(value = "new")
+    @GetMapping(value = "/new")
     public String newArticleForm() {
         return "articles/new";
     }
 
-    @PostMapping(value = "posts")
+    @GetMapping(value = "/{id}")
+    public String selectArticle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+
+        if (!optArticle.isEmpty()) {
+            model.addAttribute("article", optArticle.get());
+            return "articles/show";
+        } else {
+            return "articles/error";
+        }
+    }
+
+    @PostMapping(value = "/posts")
     public String createArticle(ArticleDto form) {
         log.info(form.toString());      // 로그 남기기
         Article article = form.toEntity();
         articleRepository.save(article);
-        return "";
+        return String.format("redirect:/articles/%d", article.getId());
     }
 }
