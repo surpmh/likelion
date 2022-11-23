15ef2fc120221f8040bf92ef76a1100b866157de
surpmh <cdd8816@gmail.com>
2022-11-23 11:07:45 +0900

    feat: 재귀 사용하지 않는 풀이 추가

--- src/main/java/com/likelion/codeup/Fibonacci.java
+++ src/main/java/com/likelion/codeup/Fibonacci.java
@@ -3,16 +3,30 @@ package com.likelion.codeup;
 import java.util.Scanner;
 
 public class Fibonacci {
-    public static int fibonacci(int n) {
-        if (n <= 2) {
-            return 1;
+    public static int fibonacci1(int n) {
+        if (n <= 1) {
+            return n;
         }
-        return fibonacci(n-2) + fibonacci(n-1);
+        return fibonacci1(n-2) + fibonacci1(n-1);
     }
+
+    public static int fibonacci2(int n) {
+        int[] arr = new int[n+1];
+
+        if (n <= 1) return n;
+
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[n] = arr[n - 2] + arr[n - 1];
+
+        return arr[n];
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        System.out.println(fibonacci(n));
+        System.out.println(fibonacci1(n));
+        System.out.println(fibonacci2(n));
     }
 }
