c5fa4c0fbef158a0e2f4d4509726e8d97c4f341c
surpmh <cdd8816@gmail.com>
2022-11-23 11:10:45 +0900

    feat: 재귀 사용하지 않는 풀이 추가

--- src/main/java/com/likelion/codeup/Fibonacci.java
+++ src/main/java/com/likelion/codeup/Fibonacci.java
@@ -17,7 +17,10 @@ public class Fibonacci {
 
         arr[0] = 0;
         arr[1] = 1;
-        arr[n] = arr[n - 2] + arr[n - 1];
+
+        for(int i=2; i<=n; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
 
         return arr[n];
     }
