8df1bad3a76d8c7320534b61dc70108b7c03bb2a
surpmh <cdd8816@gmail.com>
2022-12-12 10:26:21 +0900

    feat: (0, 0)에서 (2, 2)로가는 최소 비용

--- src/main/java/com/likelion/dp/MinCost.java
+++ src/main/java/com/likelion/dp/MinCost.java
@@ -6,11 +6,21 @@ public class MinCost {
     public static int getMinCost(int[][] matrix) {
         int[][] dp = new int[matrix.length][matrix[0].length];
 
-        for (int i=0; i<dp.length; i++) {
-            System.out.println(Arrays.toString(dp[i]));
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = matrix[i][j];
+                } else if (i == 0 && j > 0) {
+                    dp[i][j] = matrix[i][j-1] + matrix[i][j];
+                } else if (i > 0 && j == 0) {
+                    dp[i][j] = matrix[i-1][j] + matrix[i][j];
+                } else if (i > 0 && j > 0) {
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + matrix[i][j];
+                }
+            }
         }
 
-        return 0;
+        return dp[dp.length-1][dp[0].length-1];
     }
 
     public static void main(String[] args) {
